1f62f9f50c115caf27ff5091894bf5e955867646
Sandro <wrek.yamom@gmail.com>
2023-03-06 02:22:37 +0400

    js-dav4

---
+++ script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+
+
+// 1.
+
+// let array = [5, 25, 89, 120, 36];
+// array.push("javascript","python");
+// array.unshift("html","css");
+
+// console.log(array.length);
+
+// array.pop();
+// array.shift();
+
+// console.log(array);
+
+
+
+// 2.
+
+// let array = ["fortoxali","banani","msxali"];
+
+// console.log(`aris ${array.length} elementi`);
+
+// array.push("vashli","ananasi");
+// array.unshift("sazamtro");
+
+// console.log(`gaxda ${array.length} elementi`);
+
+// array.splice(2,0,"mango");
+
+// array.pop();
+// array.shift();
+
+// console.log(`darcha ${array.length} elementi`);
+
+
+
+// 3.
+
+// let array =[1, 2, 3, 4, 5];
+
+// array.splice(3,0,"a");
+// array.splice(4,0,"b");
+// array.splice(5,0,"c");
+
+// console.log(array);
+
+
+
+// 4.
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let sum = 0;
+// arr.forEach(function(i){
+//     sum += i;
+// })
+// console.log(sum);
+
+
+
+// 5.
+
+// for (let i = 100; i > 0; i--) {
+//     console.log(i);
+// }
+
+
+
+// 6.
+
+// function comparison(num1,num2){
+//     if (num1 > num2) {
+//         return "num1 is the largest";
+//     }
+//     else if (num1 < num2){
+//         return "num2 is the largest";
+//     }
+//         return "error";
+// }
+
+// let result = comparison("sandro",2);
+// console.log(result);
+
+
+// let testFunction = (num1,num2) => (num1 > num2) ? "num1 is the largest" : (num1 < num2) ? "num2 is the largest" : "error";
+
+// let result = testFunction("sandro",2);
+// console.log(result);
